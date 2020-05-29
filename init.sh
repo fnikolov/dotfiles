93b8771f2ec9122a707d838f59e1b897ea2ed34d
Filip Nikolov <filip.d.nikolov@gmail.com>
2020-05-29 21:05:10 +0300

    feat(bash): Add init script.

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install neovim tig -y
+
+ln -s ~/dotfiles/.config        ~/.config
+ln -s ~/dotfiles/.bash_profile  ~/.bash_profile
+#ln -s ~/dotfiles/.vimrc         ~/.vimrc
