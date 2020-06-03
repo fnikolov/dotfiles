c763e1e30dc7b25bbcf9b4162b012500d4165ffa
Filip Nikolov <filip.d.nikolov@gmail.com>
2020-06-03 12:31:07 +0300

    feat: Move aliases and load profile from .bashr

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,2 @@
-if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
 export VISUAL=nvim
 export EDITOR="$VISUAL"
-alias vim=/usr/bin/nvim
--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ sudo apt upgrade -y
 sudo apt install neovim tig -y
 
 ln -s ~/dotfiles/.config        ~/.config
-ln -s ~/dotfiles/.bash_profile  ~/.bash_profile
+ln -s ~/dotfiles/.bash_aliases  ~/.bash_aliases
 #ln -s ~/dotfiles/.vimrc         ~/.vimrc
+echo "source ~/dotfiles/.bash_profile" >> ~/.bashrc
