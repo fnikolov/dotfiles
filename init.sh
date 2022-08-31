4552f86572059916115e3876a0702a949311085e
Filip Nikolov <filip.d.nikolov@gmail.com>
2022-08-31 16:16:56 +0300

    feat: rhel mod

--- init.sh
+++ init.sh
@@ -1,6 +1,23 @@
-sudo apt update
-sudo apt upgrade -y
-sudo apt install neovim tig -y
+#!/bin/bash
+
+if command -v apt-get >/dev/null; then
+  echo "apt-get is used here"
+  PCK_MAN=apt
+elif command -v dnf >/dev/null; then
+  echo "dnf is used here"
+  PCK_MAN=dnf
+else
+  echo "I have no Idea what im doing here"
+  exit 1
+fi
+
+if $PCK_MAN=dnf; then
+  sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+  sudo dnf install -y neovim python3-neovim
+
+sudo $PCK_MAN update
+sudo $PCK_MAN upgrade -y
+sudo $PCK_MAN install neovim tig -y
 
 # install kubectl
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
@@ -8,9 +25,9 @@ sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 # Install kube-ps1
-mkdir -p ~/bin
-cd ~/bin
-git clone git@github.com:jonmosco/kube-ps1.git
+mkdir -p ~/.src
+cd ~/.src
+git clone https://github.com/jonmosco/kube-ps1.git
 
 ln -s ~/dotfiles/.config        ~/.config
 ln -s ~/dotfiles/.bash_aliases  ~/.bash_aliases
